1496d38b60437fbeb44193eae8d935a2fdbce0e7
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2024-08-26 01:08:25 -0600

    added comments for fonts

--- Tetris/GameText.cpp
+++ Tetris/GameText.cpp
@@ -16,9 +16,9 @@ void game_text(unsigned short i_x, unsigned short i_y, const std::string& i_text
     short character_x = i_x;
     short character_y = i_y;
     unsigned char character_width;
+//    unsigned char character_height;
     sf::Sprite character_sprite;
     sf::Texture font_texture;
-    font_texture.loadFromFile("Tetris/Resources/Images/Font.png");
     
     if (!font_texture.loadFromFile("Tetris/Resources/Images/Font.png")) {
         std::cerr << "Failed to load image. Please check the file path and name." << std::endl;
@@ -28,6 +28,7 @@ void game_text(unsigned short i_x, unsigned short i_y, const std::string& i_text
     // Calculate the width of the character based on the font image size
     // 96 because there are 96 characters in the image
     character_width = font_texture.getSize().x / 96;
+//    character_height = font_texture.getSize().y / 8;
     character_sprite.setTexture(font_texture);
 
     for (const char a : i_text)
@@ -36,12 +37,18 @@ void game_text(unsigned short i_x, unsigned short i_y, const std::string& i_text
         {
             //After every newline, increase the y-coordinate and reset the x-coordinate
             character_x = i_x;
+//            character_y += character_height;
             character_y += font_texture.getSize().y;
             continue;
         }
+        
+//        // Calculate the row and column of the character in the texture grid
+//        int column = (a - 32) % 10;
+//        int row = (a - 32) / 10;
 
         character_sprite.setPosition(character_x, character_y);
         character_sprite.setTextureRect(sf::IntRect(character_width * (a - 32), 0, character_width, font_texture.getSize().y));
+//        character_sprite.setTextureRect(sf::IntRect(column * character_width, row * character_height, character_width, character_height));
         character_x += character_width;
         i_window.draw(character_sprite);
     }
